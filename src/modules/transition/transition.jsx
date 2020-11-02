ab7a6e5780a2f1c48ac91939cef7e69ac9189563
Your Name <you@example.com>
2020-11-02 13:26:31 -0800

    better transition

--- src/modules/transition/transition.jsx
+++ src/modules/transition/transition.jsx
@@ -18,7 +18,7 @@ export default class Scroll extends React.Component {
                 <div className="magic">
                     <img src="https://res.cloudinary.com/dfulxq7so/image/upload/v1603838415/What-is-perspective-taking-basics-and-benefits-980x653_welev3.jpg"/>
                     <div className="appear">
-                        <p>Scroll over here, something'll come up!</p>
+                        {/* <p>Scroll over here, something'll come up!</p> */}
                         <img src="https://res.cloudinary.com/dfulxq7so/image/upload/v1603819869/Cover_ManageUB-Training-Deck-1_yaamsg.png"/>
                     </div>
                 </div>
